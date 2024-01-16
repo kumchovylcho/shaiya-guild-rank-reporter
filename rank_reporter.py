ed34c689c460b07ef978a7a4cf2df327b74fcb4c
Tamer <tamerchoyy@gmail.com>
2024-01-16 18:57:58 +0200

    updated for the new server and now can send messages to unlimited guilds

--- rank_reporter.py
+++ rank_reporter.py
@@ -5,11 +5,18 @@ from dotenv import dotenv_values
 
 CONFIG = dotenv_values()
 
-DISCORD_WEBHOOK = CONFIG['DISCORD_WEBHOOK']
-URL_TO_SCRAPE = CONFIG['URL_TO_SCRAPE']
-SEARCH_GUILD = CONFIG['SEARCH_GUILD']
+GUILDS_DATA = CONFIG['GUILDS']
 PLAYED_SERVER = CONFIG['PLAYED_SERVER']
-DARK_SIDE = bool(int(CONFIG['DARK_SIDE']))
+
+
+def guilds_data_as_dict(guilds_data: str):
+    as_dict = []
+    for guild in guilds_data.split(","):
+        if not guild or guild == "\n":
+            continue
+        name, side, discord_webhook = [pair.split("=")[1] for pair in guild.split(";")]
+        as_dict.append({"name": name, "is_fury": bool(int(side)), "discord_webhook": discord_webhook})
+    return as_dict
 
 
 def position_as_ordinal_number(position: str or int):
@@ -26,8 +33,8 @@ def position_as_ordinal_number(position: str or int):
     return get_naming
 
 
-def find_guild_position(rows):
-    faction_css_class = "faction-1" if DARK_SIDE else "faction-0"
+def find_guild_position(rows, look_for_guild_name: str, is_fury: bool):
+    faction_css_class = "faction_dark" if is_fury else "faction_light"
 
     position = 0
     result = {}
@@ -41,7 +48,7 @@ def find_guild_position(rows):
             position += 1
 
         guild_name = row_data[2].get_text()
-        if guild_name != SEARCH_GUILD:
+        if guild_name != look_for_guild_name:
             continue
 
         result["placement"] = position
@@ -52,36 +59,62 @@ def find_guild_position(rows):
     return result
 
 
-def send_message(message):
+def send_message(message: str, webhook: str):
     payload = {
         "content": message
     }
     json_payload = json.dumps(payload)
 
     try:
-        requests.post(DISCORD_WEBHOOK, data=json_payload, headers={"Content-Type": "application/json"})
+        requests.post(webhook, data=json_payload, headers={"Content-Type": "application/json"})
     except Exception as e:
         print(e)
 
 
-def send_discord_message():
+def send_discord_messages(guilds_data: list[dict]):
     try:
-        response = requests.get(URL_TO_SCRAPE)
-        html_as_text = response.text
+        html = """
+<tbody>
 
-        soup = BeautifulSoup(html_as_text, "html.parser")
-        guild_ranks_tbody = soup.find("table").find("tbody")
+													<tr>
+								<td>1</td>
+                                <td><div style="margin-left: 14px;" class="faction_icon faction_light" title="Alliance of Light"></div></td>
+                                <td><a>Test</a></td>
+								<td><a style="font-weight: 600;margin-top: 4px;">milSugio2</a></td> 
+                                <td style="font-weight: 600;font-size: 16px;">0</td>
+							</tr>
+														<tr>
+								<td>2</td>
+                                <td><div style="margin-left: 14px;" class="faction_icon faction_light" title="Alliance of Light"></div></td>
+                                <td><a>Uno.</a></td>
+								<td><a style="font-weight: 600;margin-top: 4px;">Genesis1</a></td> 
+                                <td style="font-weight: 600;font-size: 16px;">0</td>
+							</tr>
+														<tr>
+								<td>3</td>
+                                <td><div style="margin-left: 14px;" class="faction_icon faction_light" title="Alliance of Light"></div></td>
+                                <td><a>GUILD</a></td>
+								<td><a style="font-weight: 600;margin-top: 4px;">Fighter</a></td> 
+                                <td style="font-weight: 600;font-size: 16px;">4025</td>
+							</tr>
+													</tbody>
+        """
+
+        soup = BeautifulSoup(html, "html.parser")
+        guild_ranks_tbody = soup.find("tbody")
         all_rows = guild_ranks_tbody.find_all("tr")
 
-        guild_data = find_guild_position(all_rows)
-        side = "Fury" if DARK_SIDE else "Light"
+        for guild in guilds_data:
+            guild_data = find_guild_position(all_rows, guild["name"], guild["is_fury"])
+            side = "Fury" if guild["is_fury"] else "Light"
 
-        message = f"@here {SEARCH_GUILD} ranked " \
-                  f"{guild_data.get('placement', 'unknown')}{guild_data.get('ordinal_placement', '')} among all {side} guilds " \
-                  f"with {guild_data.get('points', '0')} points at - {PLAYED_SERVER}!"
-        send_message(message)
+            message = f"@here {guild['name']} ranked " \
+                      f"{guild_data.get('placement', 'unknown')}{guild_data.get('ordinal_placement', '')} among all {side} guilds " \
+                      f"with {guild_data.get('points', 'unknown')} points at - {PLAYED_SERVER}!"
+            send_message(message, guild["discord_webhook"])
     except Exception as e:
         print(e)
 
 
-send_discord_message()
+guilds_as_dict = guilds_data_as_dict(GUILDS_DATA)
+send_discord_messages(guilds_as_dict)
